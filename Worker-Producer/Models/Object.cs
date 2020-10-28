80735b02ae43be4ed99dc486c068063ed1f25bab
Murillo Leal <murilloadcl@msn.com>
2020-10-28 18:40:39 -0300

    Kafka Worker and Producer

---
+++ Worker-Producer/Models/Object.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Worker_Producer.Models
+{
+    class FundoInvest
+    {
+        public string FundoInvestimentos { get; set; }
+        public string Tpempres { get; set; }
+        public string CodBanco { get; set; }
+        public string Agencia { get; set; }
+        public string Conta { get; set; }
+        public string Dac10 { get; set; }
+        public DateTime Data { get; set; }
+        public string ComunicEletr { get; set; }
+        public int CodUsuario { get; set; }
+        public string OpedCana { get; set; }
+        public string CodCana { get; set; }
+        public string TipoMovi { get; set; }
+      
+
+    }
+}
---
+++ Worker-Producer/Program.cs
@@ -0,0 +1,42 @@
+﻿using Confluent.Kafka;
+using System;
+using System.Text.Json;
+using System.Threading;
+using System.Threading.Tasks;
+using Worker_Producer.Models;
+
+namespace Worker_Producer
+{
+    class Program
+    {
+        public static async Task Main()
+        {
+            var config = new ProducerConfig { BootstrapServers = "localhost:9092" };
+
+            using var p = new ProducerBuilder<Null, string>(config).Build();
+
+            {
+                try
+                {
+                    
+                    while (true)
+                    {
+                        var resultA = new FundoInvest() { FundoInvestimentos = "opc-rec-in", Tpempres = "004", CodBanco = "341", Agencia = "1403", Conta = "12222222", Dac10 = "5", Data = new DateTime(), ComunicEletr = "S", CodUsuario = 0, OpedCana = "00", CodCana = "00", TipoMovi = "N" };
+                        var resultB = JsonSerializer.Serialize(resultA);
+
+                        var dr = await p.ProduceAsync("test-topic",
+                        new Message<Null, string> { Value = $"{resultB}" });
+
+                        Console.WriteLine($"Delivered '{dr.Value}''");
+
+                        Thread.Sleep(2000);
+                    }
+                }
+                catch (ProduceException<Null, string> e)
+                {
+                    Console.WriteLine($"Delivery failed: {e.Error.Reason}");
+                }
+            }
+        }
+    }
+}
